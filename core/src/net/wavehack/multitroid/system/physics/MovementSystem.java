0eafaf0946cc019aecbba5b70cdf3bcd9241401f
WaveHack <email@wavehack.net>
2017-02-02 23:16:10 +0100

    Add MovementSystem

---
+++ core/src/net/wavehack/multitroid/system/physics/MovementSystem.java
@@ -0,0 +1,28 @@
+package net.wavehack.multitroid.system.physics;
+
+import com.artemis.Aspect;
+import com.artemis.ComponentMapper;
+import com.artemis.Entity;
+import com.artemis.systems.EntityProcessingSystem;
+import net.wavehack.multitroid.component.basic.Position;
+import net.wavehack.multitroid.component.physics.Movement;
+
+public class MovementSystem extends EntityProcessingSystem {
+
+    private ComponentMapper<Movement> mm;
+    private ComponentMapper<Position> pm;
+
+    @SuppressWarnings("unchecked")
+    public MovementSystem() {
+        super(Aspect.all(Movement.class, Position.class));
+    }
+
+    @Override
+    protected void process(Entity e) {
+        final Movement movement = this.mm.get(e);
+        final Position position = this.pm.get(e);
+
+        position.position.add(movement.velocity.cpy().scl(this.world.delta));
+    }
+
+}
