22c756846c49a9f08c5ef19e6be6215d6e0ed8bb
WaveHack <email@wavehack.net>
2017-02-02 23:10:30 +0100

    Refactor android and ios launchers

--- ios/src/net/wavehack/multitroid/IOSLauncher.java
+++ ios/src/net/wavehack/multitroid/IOSLauncher.java
@@ -1,13 +1,12 @@
 package net.wavehack.multitroid;
 
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
 import org.robovm.apple.foundation.NSAutoreleasePool;
 import org.robovm.apple.uikit.UIApplication;
 
-import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
-import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import net.wavehack.multitroid.MultitroidGame;
-
 public class IOSLauncher extends IOSApplication.Delegate {
+
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
@@ -19,4 +18,5 @@ public class IOSLauncher extends IOSApplication.Delegate {
         UIApplication.main(argv, null, IOSLauncher.class);
         pool.close();
     }
+
 }
