76aaa02ad8a22c129c3fb9428895f3841900b9fc
WaveHack <email@wavehack.net>
2017-02-02 22:38:24 +0100

    Refactor Position component

--- core/src/net/wavehack/multitroid/component/basic/Position.java
+++ core/src/net/wavehack/multitroid/component/basic/Position.java
@@ -5,14 +5,13 @@ import com.badlogic.gdx.math.Vector2;
 
 public class Position extends Component {
 
-    public Vector2 position;
+    public Vector2 position = new Vector2(0, 0);
+
+    public Position() {
+    }
 
     public Position(Vector2 position) {
         this.position = position;
-    }
-
-    public Position() {
-        this(new Vector2(0, 0));
     }
 
     public Position(float x, float y) {
